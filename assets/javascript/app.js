22ba6a819c6f3ab0d714088738317c98e4847b9e
Jakeokony1024 <jakeokony@gmail.com>
2020-01-09 15:51:56 -0600

    added questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,34 +4,34 @@ $("#start").on("click", function () {
 });
 
 var questions = [{
-    question: "",
-    answers: [],
-    correctAnswer:"",
+    question: "What year did the first episode air? ",
+    answers: ["1998", "2004", "2002", "1999"],
+    correctAnswer:"1999",
     image:
 },{
-    question: "",
-    answers: [],
-    correctAnswer:"",
+    question: "What is the name of the baby on the show? ",
+    answers: ["Chris", "Stewie", "Meg", "Brian"],
+    correctAnswer:"Stewie",
     image:
 },{
-    question: "",
-    answers: [],
-    correctAnswer:"",
+    question: "How many emmy awards has Family Guy won?",
+    answers: ["None", "Seven", "Nine", "Three"],
+    correctAnswer:"Nine",
     image:
 }, {
-    question: "",
-    answers: [],
-    correctAnswer:"",
+    question: "What is the character name of the mayor on the show?",
+    answers: ["Adam West", "Greased-up Deaf Guy", "Carter Pewterschmidt", "Joe Swanson"],
+    correctAnswer:"Adam West",
     image:
 }, {
-    question: "",
-    answers: [],
-    correctAnswer:"",
+    question: "What is the name of the dad on the show?",
+    answers: ["Clevland", "Quagmire", "Peter", "Mort"],
+    correctAnswer:"Peter",
     image:
 }, {
-    question: "",
-    answers: [],
-    correctAnswer:"",
+    question: "Who is the creator of Family Guy",
+    answers: ["David Zuckerman", "Alex Borstein", "Seth MacFarlane", "Seth Green"],
+    correctAnswer:"Seth MacFarlane",
     image:
 }
 }];
