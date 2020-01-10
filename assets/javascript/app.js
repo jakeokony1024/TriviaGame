ec8fa8d2ac6f4e3c6b29c6e38b070dc385a362c1
Jakeokony1024 <jakeokony@gmail.com>
2020-01-09 18:14:27 -0600

    checked for function working need fix still

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,96 @@
 $("#start").on("click", function () {
     $("#start").remove();
-    console.log(questions)
+    game.loadQuestion();
 
 });
 
+$(document).on('click', 'answer-button', function(e){
+    game.clickedOn(e);
+});
+
 var questions = [{
-    question: "What year did the first episode air? ",
+    question: "What year did the first Family Guy episode air? ",
     answers: ["1998", "2004", "2002", "1999"],
-    correctAnswer:"1999",
+    correctAnswer: "1999",
     image: "./assets/images/FG-firstepisode.jpg"
 }, {
     question: "What is the name of the baby on the show? ",
     answers: ["Chris", "Stewie", "Meg", "Brian"],
-    correctAnswer:"Stewie",
+    correctAnswer: "Stewie",
     image: "./assets/images/stewie.jpg"
 }, {
     question: "How many emmy awards has Family Guy won?",
     answers: ["None", "Seven", "Nine", "Three"],
-    correctAnswer:"Nine",
-    image:"./assets/images/emmy-award.jpg"
+    correctAnswer: "Nine",
+    image: "./assets/images/emmy-award.jpg"
 }, {
     question: "What is the character name of the mayor on the show?",
     answers: ["Adam West", "Greased-up Deaf Guy", "Carter Pewterschmidt", "Joe Swanson"],
-    correctAnswer:"Adam West",
+    correctAnswer: "Adam West",
     image: "./assets/images/adam-west.jpg"
 }, {
     question: "What is the name of the dad on the show?",
     answers: ["Clevland", "Quagmire", "Peter", "Mort"],
-    correctAnswer:"Peter",
+    correctAnswer: "Peter",
     image: "./assets/images/peter.gif"
 }, {
     question: "Who is the creator of Family Guy",
     answers: ["David Zuckerman", "Alex Borstein", "Seth MacFarlane", "Seth Green"],
-    correctAnswer:"Seth MacFarlane",
+    correctAnswer: "Seth MacFarlane",
     image: "./assets/images/seth-macfarlane-medium.jpg"
 
-}]; 
+}];
 
 var game = {
-    questions1: questions, 
+    questions1: questions,
     currentQuestion: 0,
-    counter:30,
-    correct:0,
-    incorrect:0,
-    countdown: function(){
+    counter: 30,
+    correct: 0,
+    incorrect: 0,
+    countdown: function () {
+        game.counter--;
+        $("#counter").html(game.counter);
+        if (game.counter <= 0) {
+            console.log("TIMES UP");
+            game.timesUp();
+        }
+    },
+    loadQuestion: function () {
+        timer = setInterval(game.countdown, 1000);
+        $('#subWrapper').html('<h2>' + questions[game.currentQuestion].question + '</h2>');
+        for (var i = 0; i < questions[game.currentQuestion].answers.length; i++) {
+            $("#subWrapper").append('<button class="answer-button" id="button-' + i + '" data-name="' + questions[game.currentQuestion].answers[i] + '">' + questions[game.currentQuestion].answers[i] + '</button>');
+        }
 
     },
-    loadQuestion: function(){
+    nextQuestion: function () {
+        game.counter = 30;
+        $('#counter').html(game.counter);
+        game.currentQuestion++;
+        game.loadQuestion();
+    },
+    timesUp: function () {
 
     },
-    nextQuestion: function(){
+    results: function () {
 
     },
-    timesUp: function(){
-
+    clickedOn: function (e) {
+        clearInterval(timer);
+        if ($(e.target).data("name")==questions[game.currentQuestion].correctAnswer){
+            game.answeredCorrectly();
+        }
+        else {
+            game.answeredIncorrectly();
+        }
     },
-    results: function(){
-
+    answeredCorrectly: function () {
+        console.log("YOU GOT IT");
     },
-    clickedOn: function(){
-
+    answeredIncorrectly: function () {
+        console.log("YOU GOT IT");
     },
-    answeredCorrectly: function(){
-
-    },
-    answeredIncorrectly: function(){
-
-    },
-    reset: function(){
-
+    reset: function () {
+        
     }
 }
